f669940b9beb3367352ce0ba44075925cf5db3f7
JAIDEV YADAV <jaiy9956@gmail.com>
2026-07-16 15:20:19 +0530

    Time: 1 ms (43.96%), Space: 9.9 MB (82.43%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
-    void solve(int index, vector<int>& nums, vector<int>& ds,
-               vector<vector<int>>& ans) {
-        if (index == nums.size()) {
-            ans.push_back(ds);
-            return;
-        }
+    // void solve(int index, vector<int>& nums, vector<int>& ds,
+    //            vector<vector<int>>& ans) {
+    //     if (index == nums.size()) {
+    //         ans.push_back(ds);
+    //         return;
+    //     }
 
-        // Pick
-        ds.push_back(nums[index]);
-        solve(index + 1, nums, ds, ans);
+    //     // Pick
+    //     ds.push_back(nums[index]);
+    //     solve(index + 1, nums, ds, ans);
 
-        // Not Pick
-        ds.pop_back();
-        solve(index + 1, nums, ds, ans);
-    }
+    //     // Not Pick
+    //     ds.pop_back();
+    //     solve(index + 1, nums, ds, ans);
+    // }
 
     vector<vector<int>> subsets(vector<int>& nums) {
+        // vector<vector<int>> ans;
+        // vector<int> ds;
+        // solve(0, nums, ds, ans);
+        // return ans;
+        int n=nums.size();
+        int subset = 1<<n;
         vector<vector<int>> ans;
-        vector<int> ds;
-        solve(0, nums, ds, ans);
+        for(int num=0;num<subset;num++){
+            vector<int> lst;
+            for(int i=0;i<n;i++){
+                if(num & (1<<i)){
+                    lst.push_back(nums[i]);
+                }
+            }
+            ans.push_back(lst);
+        }
         return ans;
+
+
     }
 };
