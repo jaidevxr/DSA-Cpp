65aefbb211bc33b60cd0a1d8771288698a9a3933
JAIDEV YADAV <jaiy9956@gmail.com>
2026-09-06 00:36:37 +0530

    Time: 333 ms (95.72%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,17 @@
+WITH DailySum AS (
+    SELECT visited_on, SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+)
+SELECT 
+    visited_on,
+    SUM(amount) OVER(
+        ORDER BY visited_on 
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS amount,
+    ROUND(AVG(amount) OVER(
+        ORDER BY visited_on 
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ), 2) AS average_amount
+FROM DailySum
+LIMIT 999999 OFFSET 6;
